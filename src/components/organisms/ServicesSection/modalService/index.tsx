6a66f6aaeb5515c030b4c7952bf1fa03e2efefc0
sirdav1d <ddavid.diniz@gmail.com>
2023-11-09 18:10:45 -0300

    fix: :lipstick: ajuste de card - centralização

--- src/components/organisms/ServicesSection/modalService/index.tsx
+++ src/components/organisms/ServicesSection/modalService/index.tsx
@@ -27,8 +27,8 @@ export default function ModalService(props: CardServsProps) {
 				variants={ModalAnimation}
 				animate={`${show ? 'visible' : 'closed'}`}
 				initial='closed'
-				className=' bg-white/40 backdrop-blur-sm h-screen transform w-screen overflow-hidden fixed z-50 top-0 left-0 bottom-0 flex mx-auto transition-all duration-300 ease-linear'>
-				<div className='bg-white flex flex-col mx-5 lg:w-1/3 lg:h-[54%] m-auto shadow-xl rounded-lg relative z-50 p-5'>
+				className=' bg-white/40 backdrop-blur-sm h-screen transform w-screen overflow-hidden fixed z-50 top-0 left-0 bottom-0 flex mx-auto transition-all duration-300 ease-linear items-center justify-center '>
+				<div className='bg-white flex flex-col mx-5  lg:w-1/3 lg:h-[54%] m-auto shadow-xl rounded-lg relative z-50 p-5 '>
 					<button
 						onClick={() => setShow(false)}
 						className=' w-20 h-20 absolute right-1 top-1 rotate-45 text-6xl text-red-600 hover:scale-110 duration-100 transition-all ease-linear'>
@@ -39,7 +39,7 @@ export default function ModalService(props: CardServsProps) {
 						alt={props.title}
 						className='w-full object-cover h-44 object-center rounded-md'
 					/>
-					<div className='mt-3 flex flex-col justify-between h-full'>
+					<div className='mt-3 flex flex-col justify-between h-full '>
 						<h3 className='font-bold text-xl text-stone-900'>{props.title}</h3>
 						<p className='mt-2 text-base text-stone-800'>{props.description}</p>
 						<CTAButton text={'Falar Com Consultor'} />
